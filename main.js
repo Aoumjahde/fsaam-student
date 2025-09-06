63521ced42b1e1fac5d6d68fe531fbcdfc82082e
Aziz Oumjahde <aziz.dev@gmail.com>
2025-09-06 13:01:52 +0100

    fixing the navbar styling bug

--- main.js
+++ main.js
@@ -2,5 +2,5 @@
 
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
-    ('window-scroll', window.scrollY > 100)
+    ('window-scroll', window.scrollY > 50)
 })
