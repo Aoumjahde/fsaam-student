5ebd5078d95a130d3515ee028c8996129c920b73
Aziz Oumjahde <aziz.dev@gmail.com>
2025-09-08 15:35:03 +0100

    fixing show/hidden fgas section

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 50)
 })
+
+const fags = document.querySelectorAll('.fag');
+
+fags.forEach(faq =>{
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
+    })
+})
